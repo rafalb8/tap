5d0eec16f26905601edccac0063ae841dd68eaf4
Rafalb8 <rafalb8@hotmail.com>
2026-05-13 23:44:56 +0200

    Add config

---
+++ internal/config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"flag"
+	"os"
+	"path/filepath"
+	"slices"
+
+	"github.com/elazarl/goproxy"
+)
+
+// Flags
+var (
+	Json   bool
+	Simple bool
+	Cert   string
+)
+
+var (
+	Name string
+	Args []string
+)
+
+func init() {
+	flag.BoolVar(&Json, "json", false, "json mode")
+	flag.BoolVar(&Simple, "simple", false, "simple mode")
+	flag.StringVar(&Cert, "cert", filepath.Join(os.TempDir(), "tap-ca.pem"), "proxy cert")
+
+	flags := os.Args[1:]
+	split := slices.Index(os.Args, "--")
+	if split != -1 {
+		flags = os.Args[1:split]
+		Name = os.Args[split+1]
+		Args = os.Args[split+2:]
+	}
+
+	err := flag.CommandLine.Parse(flags)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = os.Stat(Cert)
+	if os.IsNotExist(err) {
+		err = os.WriteFile(Cert, goproxy.CA_CERT, 0o644)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
