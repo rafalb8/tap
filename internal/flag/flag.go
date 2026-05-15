2ca4035212ba469d1aa9802ee773c0f02d8fd832
Rafalb8 <rafalb8@hotmail.com>
2026-05-15 22:56:05 +0200

    Add support for custom cert

--- internal/flag/flag.go
+++ internal/flag/flag.go
@@ -1,6 +1,7 @@
 package flag
 
 import (
+	"crypto/tls"
 	"os"
 	"path/filepath"
 	"slices"
@@ -15,8 +16,9 @@ var (
 	Simple  bool
 	Verbose bool
 
-	Cert   string
 	Output string
+
+	CertFile string
 )
 
 var (
@@ -25,11 +27,13 @@ var (
 )
 
 func init() {
+	defCertDir := filepath.Join(os.TempDir(), "tap")
+
 	pflag.BoolVarP(&Json, "json", "j", false, "json mode")
 	pflag.BoolVarP(&Simple, "simple", "s", false, "simple mode")
 	pflag.BoolVarP(&Verbose, "verbose", "v", false, "print all headers (json/default mode only)")
-	pflag.StringVar(&Cert, "cert", filepath.Join(os.TempDir(), "tap-ca.pem"), "proxy cert")
 	pflag.StringVarP(&Output, "output", "o", "", "output path; enables std outputs")
+	certDir := pflag.String("cert-dir", defCertDir, "proxy cert directory")
 
 	flags := os.Args[1:]
 	split := slices.Index(os.Args, "--")
@@ -44,11 +48,46 @@ func init() {
 		panic(err)
 	}
 
-	_, err = os.Stat(Cert)
+	CertFile = filepath.Join(*certDir, "ca.crt")
+
+	if *certDir == defCertDir {
+		saveCert(*certDir)
+	} else {
+		loadCert(*certDir)
+	}
+}
+
+func saveCert(dir string) {
+	err := os.MkdirAll(dir, 0o700)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = os.Stat(CertFile)
 	if os.IsNotExist(err) {
-		err = os.WriteFile(Cert, goproxy.CA_CERT, 0o644)
+		err = os.WriteFile(CertFile, goproxy.CA_CERT, 0o644)
 		if err != nil {
 			panic(err)
 		}
 	}
+}
+
+func loadCert(dir string) {
+	_, err := os.Stat(CertFile)
+	if err != nil {
+		panic("loadCert: certFile: " + err.Error())
+	}
+
+	keyFile := filepath.Join(dir, "ca.key")
+	_, err = os.Stat(keyFile)
+	if err != nil {
+		panic("loadCert: keyFile: " + err.Error())
+	}
+
+	cert, err := tls.LoadX509KeyPair(CertFile, keyFile)
+	if err != nil {
+		panic("loadCert: failed: " + err.Error())
+	}
+
+	goproxy.GoproxyCa = cert
 }
